1a6b6aa8c04ab022eaad5bcc52464d7501c8168e
fanisvl <fanis.vlahogiannis@gmail.com>
2023-05-16 11:51:21 +0300

    Delete app.java

--- Main.java
+++ Main.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-// ALTERNATIVE TO APP.java
 public class Main {
     public static void main(String[] args) {
         ArrayList<Product> availableProducts = new ArrayList<>();
