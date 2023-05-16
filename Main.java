57aa1c9a6aee4a9775e3467e85e156bdabdaf279
fanisvl <fanis.vlahogiannis@gmail.com>
2023-05-16 11:46:19 +0300

    create and add 2tvs to availableProducts

--- Main.java
+++ Main.java
@@ -3,9 +3,15 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         ArrayList<Product> availableProducts = new ArrayList<>();
+
+        // Create & add Tvs
         Tv lcd_tv = new Tv(32, Tv.TvModels.LCD, "a321", 2017, "LG", 299.99,
                 15, "42'", "1440p", Tv.TvPorts.HDMI);
-
+        Tv led_tv = new Tv(12, Tv.TvModels.LED, "a321", 2015, "LG", 199.99,
+                10, "42'", "1080p", Tv.TvPorts.DVI);
+        availableProducts.add(lcd_tv);
+        availableProducts.add(led_tv);
+        // Create & add VideoPlayers
 
     }
 }
