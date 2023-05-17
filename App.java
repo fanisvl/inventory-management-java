868f16d7b3b11869467ee0e1093b6f7e1fa95144
fanisvl <fanis.vlahogiannis@gmail.com>
2023-05-17 21:38:24 +0300

    add menu logic to App

--- App.java
+++ App.java
@@ -1,21 +1,61 @@
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
+        ArrayList<Order> orders = new ArrayList<>();
+        ArrayList<Sale> sales = new ArrayList<>();
         // Why a hashmap?
         HashMap<ProductModel, Product> availableProducts = new HashMap<>();
-        // Fill availableProducts with every model
         // (fulfills requirement of at least models 2 of every type
         // (?) Washer has only one model??? Different energyClass, capacity or rpm count as different models?)
         // Maybe replace with manual creation of Model objects
+
+        // Fill availableProducts with every model
         for(ProductModel model : ProductModel.values()) {
             availableProducts.put(model, new Product(30, model));
         }
 
         System.out.println(availableProducts);
 
-        for(ProductModel model : GamingConsole.models) {
-            System.out.println(model);
-            System.out.println(availableProducts.get(model).getAvailablePieces());
+//        for(ProductModel model : GamingConsole.models) {
+//            System.out.println(model);
+//            System.out.println(availableProducts.get(model).getAvailablePieces());
+//        }
+        // MENU
+        Scanner input = new Scanner(System.in);
+        int choice;
+        while (true) {
+            System.out.println("\n Inventory App\n");
+            System.out.println("1. Available Products");
+            System.out.println("2. Orders");
+            System.out.println("3. Sales");
+            System.out.println("0. Exit");
+            System.out.print("Enter Selection: ");
+
+            choice = input.nextInt();
+            input.nextLine();
+
+            switch(choice) {
+                case 1 -> {
+                    System.out.println("\nAVAILABLE PRODUCTS\n");
+
+                }
+                case 2 -> {
+                    System.out.println("ORDERS");
+                }
+                case 3 -> {
+                    System.out.println("SALES");
+                }
+                case 0 -> {
+                    System.out.println("\nExiting program..");
+                    System.exit(0);
+                }
+                default -> {
+                    System.out.println("Invalid Menu Option");
+                }
+            }
         }
     }
 }
--- Main.java
+++
@@ -1,9 +0,0 @@
-import java.util.ArrayList;
-// ALTERNATIVE TO APP.java
-public class Main {
-    public static void main(String[] args) {
-        ArrayList<Product> availableProducts = new ArrayList<>();
-
-
-    }
-}
